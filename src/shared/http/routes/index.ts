5fdf2180c1a436d94a8606ddee62289762740541
joaovic499 <joaovictorsilva60@Live.com>
2024-05-26 12:56:53 -0300

    Adicionando midlewares de exceções de erros

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -3,6 +3,7 @@ import { Router } from 'express'
 const routes = Router()
 
 routes.get('/', (req, res) => {
+  throw new Error('Acesso Negado')
   return res.json({ message: 'Olá dev' })
 })
 
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,8 +1,9 @@
 import 'dotenv/config'
-import express from 'express'
+import express, { Request, Response, NextFunction } from 'express'
 import 'express-async-errors'
 import cors from 'cors'
 import { routes } from './routes'
+import { AppError } from '@shared/errors/AppError'
 
 const app = express()
 
@@ -10,6 +11,21 @@ app.use(express.json())
 app.use(cors())
 
 app.use(routes)
+
+app.use((error: Error, _req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: 'error',
+      message: error.message,
+    })
+  }
+  console.log(error)
+
+  return res.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  })
+})
 
 app.listen(process.env.PORT, () => {
   console.log(`Server aberta no ${process.env.PORT}`)
